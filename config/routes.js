8482b595f99c60dc426ca8feb3a4b40350db4a42
WajahatAliAbid <wajahatckl@outlook.com>
2019-07-02 23:53:38 +0500

    remvoed verbose comments

--- config/routes.js
+++ config/routes.js
@@ -12,8 +12,6 @@ fs.readdirSync(routerPath)
         return (file.indexOf(".") !== 0) && (file !== "index.js") && (file.slice(-3) === ".js");
     })
     .forEach(function (file) {
-        //var routeName = '/' + file.slice(0,-3);
-        //app.use(routeName, require(path.join(__dirname,file)));
         require(path.join(routerPath, file))(router);
     });
 
